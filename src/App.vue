5480d08ae1f1e18e92b953500758bb38e114dc82
Steavn Chen <tom9876555@yahoo.com.tw>
2022-08-08 23:44:19 +0800

    git init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="sass">
+  @import "./styles/global.sass"
+</style>
+
+<script>
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+export default {
+  name: 'App'
+}
+</script>
+
+
